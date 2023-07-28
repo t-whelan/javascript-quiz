26238b97865d22c01b7e151a4bd95c5332f2ea37
Tionne Whelan <80913553+t-whelan@users.noreply.github.com>
2023-07-28 13:40:11 +0200

    js edits

---
+++ js/questions.js
@@ -0,0 +1,119 @@
+
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is the correct way to declare a variable?",
+    answer: "let myVar = 10;",
+    options: [
+      "var my Myvar =10;",
+      "variable myVar = 10;",
+      "let myVar = 10;",
+      "const myVar = 10;"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which function is used to print Something to the console?",
+    answer: "console.log()",
+    options: [
+      "print()",
+      "console.log()",
+      "log()",
+      "display()"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What will be the result of the following: var x = 5; console.log(x + '2')?",
+    answer: " '52' ",
+    options: [
+      "52",
+      "7",
+      " '52' ",
+      "6"
+    ]
+  },
+  {
+    numb: 4,
+    question: "How do you check if a variable is a type string 'string' in Javascript?",
+    answer: "typeof(myVar)==='string'",
+    options: [
+      "myVar.type==''string'",
+      "typeof(myVar)==='string'",
+      "typeof myVar==='string'",
+      "myVar==='string'"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does the '=== operator' do in JavaScript?",
+    answer: " Checks if two values are equal in both value and data type",
+    options: [
+      "Assigns a value to a variable",
+      "Checks if two values are equal in both value and data type",
+      "Performs a logical OR operation",
+      "Compares two values for inequality"
+    ]
+  },
+ 
+  {
+    numb: 6,
+    question: "How do you add a comment in JavaScript?",
+    answer: "// This is a comment",
+    options: [
+      "/* This is a comment */",
+      "//This is a comment",
+      "-- This is a comment --",
+      "'This is a comment'",
+    ],
+  },
+  {
+    numb: 7,
+    question: "What is the purpose of the 'typeof' operator?",
+    answer: "To get the data type of a variable",
+    options: [
+      "To check if a variable is defined",
+      "To perform mathematical operations",
+      "To get the data type of a variable",
+      "To create a new variable",
+    ],
+  },
+  {
+    numb: 8,
+    question: "Which array method is used to add elements to the end of an array?",
+    answer: "push()",
+    options: [
+      "append()",
+      "insert()",
+      "add()",
+      "push()",
+    ],
+  },
+  {
+    numb: 9,
+    question: "What is the purpose of the 'return' statement in a function?",
+    answer: "To specify the value that the function returns",
+    options: [
+      "To end the function's execution",
+      "To print a value to the console",
+      "To specify the value that the function returns",
+      "To declare a new variable",
+    ],
+  },
+  {
+    numb: 10,
+    question: "Which built-in method can be used to convert a string to uppercase?",
+    answer: "toUpperCase()",
+    options: [
+      "toUppercase()",
+      "toUpperCase()",
+      "uppercase()",
+      "upper()",
+    ],
+  },
+];
+  
+
+
+
